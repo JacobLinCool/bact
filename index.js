2ec6795510ab63510855c0f933fdbbc3a788bae4
JacobLinCool <jacoblincool@gmail.com>
2021-07-02 01:10:04 +0800

    ...

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const fetch = require("node-fetch");
 const { argv } = require("process");
 
-main(argv[2] || 15357, argv[3] || 100);
+main(argv[2] || 15357, parseInt(argv[3] || 100));
 
 async function main(sn, time) {
     console.log(sn, time);
-    let url = "https://ani.gamer.com.tw/ajax/videoStart.php?sn=" + sn;
 
     let count = 0;
     while (time--) {
-        count += await fetch(url).then((r) => r.ok);
+        let nologinuser = await fetch("https://ani.gamer.com.tw/ajax/getdeviceid.php").then((r) => r.headers.get("set-cookie").split(";")[0].split("=")[1]);
+        count += await fetch("https://ani.gamer.com.tw/ajax/videoStart.php?sn=" + sn, {
+            headers: {
+                cookie: "nologinuser=" + nologinuser,
+            },
+        }).then((r) => r.ok);
         console.log(count);
         await sleep(500);
     }
